b514da141be7da9b7a88a2fc4363ffd5c9f4f7f3
Ashmit2520 <105515137+Ashmit2520@users.noreply.github.com>
2025-08-05 21:56:05 -0500

    Updating the homepage UI

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-[80vh] px-4">
-      <h1 className="text-4xl md:text-5xl font-bold mb-6 text-center">UW-Madison Smart Course Selector</h1>
-      <p className="text-lg md:text-xl mb-8 text-gray-700 text-center max-w-xl">Find the perfect courses for you—powered by AI.</p>
+    <main className="flex flex-col bg-slate-950 items-center justify-center min-h-[80vh] px-4">
+      <h1 className="text-4xl md:text-5xl font-bold mb-6 text-center">SiftAI</h1>
+      <p className="text-lg md:text-xl mb-8 text-gray-400 text-center max-w-xl">Find the perfect courses for you - powered by AI.</p>
       <a
         href="/chat"
-        className="px-8 py-3 bg-blue-600 text-white rounded-xl shadow hover:bg-blue-700 transition"
+        className="px-8 py-3 bg-violet-400 text-slate-950 rounded-xl shadow hover:bg-violet-500 transition"
       >
         Get Started
       </a>
